c55b6cc363795faeb91b1d45cbfc01a5a835a80a
Seung Park <ps712@naver.com>
2022-07-25 10:05:43 +0900

    test

--- section2/2-3-1-download.py
+++ section2/2-3-1-download.py
@@ -1,4 +1,4 @@
-import urllib.request
+import urllib.request as req
 from urllib.parse import urlparse
 import sys
 import io
@@ -9,13 +9,20 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 url = "http://www.encar.com/"
 
-mem = urllib.request.urlopen(url)
+mem = req.urlopen(url)
 
 print(type(mem))
+print('--------------------')
 print("geturl :",mem.geturl())
+print('--------------------')
 print("status :",mem.status)
+print('--------------------')
 print("headers :",mem.getheaders())
+print('--------------------')
 print("info :",mem.info(),"\n")
+print('--------------------')
 print("getcode :",mem.getcode())
+print('--------------------')
 print("read :",mem.read(10).decode('utf-8'))
+print('--------------------')
 print(urlparse('http://www.encar.co.kr?test=test').query)
